e0963dbca5114daff7477d0bb0377a19a64340ff
NDLong <longnd1@bravebits.vn>
2023-08-12 12:35:51 +0700

    Add vercel.json

--- server.js
+++ server.js
@@ -13,6 +13,12 @@ const app = express();
 app.use(express.json()); //for json
 app.use(cors());
 
+app.all("/*", function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  next();
+});
+
 //router
 app.use("/v1/api", userRouter);
 
