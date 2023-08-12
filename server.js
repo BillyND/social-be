1cef38506a9a517cfb7191a8fddd81fe12a1bfe6
NDLong <longnd1@bravebits.vn>
2023-08-12 13:02:16 +0700

    update connection

--- server.js
+++ server.js
@@ -1,18 +1,13 @@
+require("dotenv").config();
 const express = require("express");
-const cors = require("cors");
-const dotenv = require("dotenv");
+const port = process.env.PORT || 3000;
+const hostname = process.env.HOST_NAME;
 const { connection } = require("./src/config/database");
 const userRouter = require("./src/routes/UserRoutes");
-dotenv.config();
-
-const hostname = process.env.HOST;
-const port = process.env.PORT;
 
 const app = express();
 
-//config req.body
-app.use(express.json()); //for json
-app.use(express.urlencoded({ extended: true })); //for form data
+const cors = require("cors");
 
 app.use(cors());
 
@@ -22,7 +17,11 @@ app.all("/*", function (req, res, next) {
   next();
 });
 
-//router
+//config req.body
+app.use(express.json()); //for json
+app.use(express.urlencoded({ extended: true })); //for form data
+
+//routes
 app.use("/v1/api", userRouter);
 
 app.get("/", (req, res) => {
@@ -34,10 +33,10 @@ let testConnect = async () => {
   try {
     await connection();
     app.listen(port, hostname, () => {
-      console.log(`>>> App running on http://${hostname}:${port}`);
+      console.log(`Example app listening on http://${hostname}:${port}`);
     });
   } catch (error) {
-    console.log(">>>> Error", error);
+    console.log(">>>>error", error);
   }
 };
 testConnect();
--- src/config/database.js
+++ src/config/database.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 const mongoose = require("mongoose");
 
-const dbState = [
+var dbState = [
   {
     value: 0,
     label: "disconnected",
@@ -21,12 +21,18 @@ const dbState = [
 ];
 
 const connection = async () => {
+  const options = {
+    user: process.env.DB_USER,
+    pass: process.env.DB_PASSWORD,
+    dbName: process.env.DB_NAME,
+  };
   try {
-    await mongoose.connect(process.env.DB_HOST);
+    await mongoose.connect(process.env.DB_HOST, options);
     const state = Number(mongoose.connection.readyState);
-    console.log(">>>", dbState.find((f) => f.value == state).label, "to db");
+    console.log(dbState.find((f) => f.value == state).label, "to db");
+    // connected to db
   } catch (error) {
-    console.log(">>>Error", error);
+    console.log(">>>error connection", error);
   }
 };
 
