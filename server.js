e8ae608485748c7c4760bfe8fe127ad8d44096b2
NDLong <longnd1@bravebits.vn>
2023-08-13 18:29:07 +0700

    test disable routes

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const port = process.env.PORT || 3000;
 const hostname = process.env.HOST_NAME;
 const { connection } = require("./src/config/database");
 // const userRouter = require("./src/routes/UserRoutes");
-const articleRouter = require("./src/routes/ArticleRoutes");
+// const articleRouter = require("./src/routes/ArticleRoutes");
 
 const app = express();
 
