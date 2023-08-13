0545b09c5cd59b9aa4f938be7d9d616a4fc792ca
NDLong <longnd1@bravebits.vn>
2023-08-13 18:59:49 +0700

    enable full article routes

--- src/routes/articlesRoutes.js
+++ src/routes/articlesRoutes.js
@@ -1,22 +1,22 @@
 const express = require("express");
-// const verifyJWT = require("../middleWare/verifyJWT");
-// const {
-//   createArticle,
-//   updateArticle,
-//   deleteArticle,
-//   getAllArticles,
-//   getArticleByAuthor,
-// } = require("../controllers/articleController");
+const verifyJWT = require("../middleWare/verifyJWT");
+const {
+  createArticle,
+  updateArticle,
+  deleteArticle,
+  getAllArticles,
+  getArticleByAuthor,
+} = require("../controllers/articleController");
 
 const router = express.Router();
 
-// router.post("/create-article", verifyJWT, createArticle);
+router.post("/create-article", verifyJWT, createArticle);
 
-// router.put("/update-article/:articleId", verifyJWT, updateArticle);
+router.put("/update-article/:articleId", verifyJWT, updateArticle);
 
-// router.delete("/delete-article/:articleId", verifyJWT, deleteArticle);
+router.delete("/delete-article/:articleId", verifyJWT, deleteArticle);
 
-// router.get("/all-articles", getAllArticles);
+router.get("/all-articles", getAllArticles);
 
 router.get("/test", (req, res) => {
   res.send("Test article routes");
