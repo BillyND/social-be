125a177c63916a7c4cd4726c5b85020783fac483
Billy <longnd1@bravebits.vn>
2023-08-26 20:28:47 +0700

    update server.js

--- server.js
+++ server.js
@@ -1,48 +1,44 @@
-try {
-  require("dotenv").config();
-  const express = require("express");
-  const port = process.env.PORT || 3000;
-  const hostname = process.env.HOST_NAME;
-  const { connection } = require("./src/config/database");
-  const app = express();
+require("dotenv").config();
+const express = require("express");
+const port = process.env.PORT || 3000;
+const hostname = process.env.HOST_NAME;
+const { connection } = require("./src/config/database");
+const app = express();
 
-  const userRoutes = require("./src/routes/user");
-  const postRoutes = require("./src/routes/post");
+const userRoutes = require("./src/routes/user");
+const postRoutes = require("./src/routes/post");
 
-  const cors = require("cors");
+const cors = require("cors");
 
-  app.use(cors());
+app.use(cors());
 
-  app.all("/*", function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "X-Requested-With");
-    next();
-  });
+app.all("/*", function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  next();
+});
 
-  //config req.body
-  app.use(express.json()); //for json
-  app.use(express.urlencoded({ extended: true })); //for form data
+//config req.body
+app.use(express.json()); //for json
+app.use(express.urlencoded({ extended: true })); //for form data
 
-  //routes
-  app.use("/api", userRoutes);
-  app.use("/api", postRoutes);
+//routes
+app.use("/api", userRoutes);
+app.use("/api", postRoutes);
 
-  app.get("/", (req, res) => {
-    res.send("Api social running ");
-  });
+app.get("/", (req, res) => {
+  res.send("Api social running ");
+});
 
-  // testConnection
-  let testConnect = async () => {
-    try {
-      await connection();
-      app.listen(port, hostname, () => {
-        console.log(`Example app listening on http://${hostname}:${port}`);
-      });
-    } catch (error) {
-      console.log(">>>>error", error);
-    }
-  };
-  testConnect();
-} catch (error) {
-  console.log(">>>error:", error);
-}
+// testConnection
+let testConnect = async () => {
+  try {
+    await connection();
+    app.listen(port, hostname, () => {
+      console.log(`Example app listening on http://${hostname}:${port}`);
+    });
+  } catch (error) {
+    console.log(">>>>error", error);
+  }
+};
+testConnect();
