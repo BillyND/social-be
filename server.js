2ff59d547f26c925798b3b316aa78f6e5a63178a
Billy <longnd1@bravebits.vn>
2023-08-26 20:37:44 +0700

    disable routes

--- server.js
+++ server.js
@@ -5,8 +5,8 @@ const hostname = process.env.HOST_NAME;
 const { connection } = require("./src/config/database");
 const app = express();
 
-const userRoutes = require("./src/routes/user");
-const postRoutes = require("./src/routes/post");
+// const userRoutes = require("./src/routes/user");
+// const postRoutes = require("./src/routes/post");
 
 const cors = require("cors");
 
@@ -23,8 +23,8 @@ app.use(express.json()); //for json
 app.use(express.urlencoded({ extended: true })); //for form data
 
 //routes
-app.use("/api", userRoutes);
-app.use("/api", postRoutes);
+// app.use("/api", userRoutes);
+// app.use("/api", postRoutes);
 
 app.get("/", (req, res) => {
   res.send("Api social running ");
