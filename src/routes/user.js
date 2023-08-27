3d419da25adc11b09138da8a81b56fc35f290076
Billy <longnd1@bravebits.vn>
2023-08-28 00:53:23 +0700

    enable router register

--- src/routes/user.js
+++ src/routes/user.js
@@ -11,7 +11,7 @@ router.get("/users/:id", userController.getUserById);
 // router.post("/login", userController.loginUser);
 
 //REGISTER
-// router.post("/register", userController.registerUser);
+router.post("/register", userController.registerUser);
 
 // router.post("/change-password", userController.changePassword);
 // router.post("/delete-user", userController.softDeleteUser);
