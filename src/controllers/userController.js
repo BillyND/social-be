94afd6da1344f5663adcbae76a41476dee267386
Billy <longnd1@bravebits.vn>
2023-08-28 00:48:26 +0700

    remove optional chaining

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -7,7 +7,7 @@ const userController = {
   //GET A USER
   getUserById: async (req, res) => {
     try {
-      const user = await User.findById(req?.params?.id);
+      const user = await User.findById(req.params.id);
       res.status(200).json(user);
     } catch (err) {
       res.status(500).json(err);
