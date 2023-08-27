c09a38160704118b289c6346db5c8b05ebf970f7
Billy <longnd1@bravebits.vn>
2023-08-28 00:19:29 +0700

    enable import bcrypt

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,69 +1,69 @@
-// var bcrypt = require("bcryptjs");
+var bcrypt = require("bcryptjs");
 // const jwt = require("jsonwebtoken");
 // const { regexEmail, regexPassword } = require("../constant");
 const User = require("../models/User");
 
 const userController = {
   //GET A USER
-  getUserById: async (req, res) => {
-    try {
-      const user = await User.findById(req.params.id);
-      res.status(200).json(user);
-    } catch (err) {
-      res.status(500).json(err);
-    }
-  },
-  //REGISTER
-  registerUser: async (req, res) => {
-    // try {
-    // const { email, password } = req?.body;
-    // const trimmedEmail = email?.trim();
-    // const trimmedPassword = password?.trim();
-    // const isEmailValid = trimmedEmail && regexEmail.test(trimmedEmail);
-    // const isPasswordValid =
-    //   trimmedPassword && trimmedPassword.match(regexPassword);
-    // if (!isEmailValid) {
-    //   return res.status(400).json({
-    //     errCode: 1,
-    //     message: "Invalid email format!",
-    //   });
-    // }
-    // if (!isPasswordValid) {
-    //   return res.status(400).json({
-    //     errCode: 1,
-    //     message: "Password must be at least 6 characters long.",
-    //   });
-    // }
-    // const salt = await bcrypt.genSaltSync(10);
-    // const hashPassword = await bcrypt.hashSync(trimmedPassword, salt);
-    // const atIndex = trimmedEmail.indexOf("@");
-    // const userName = atIndex !== -1 ? trimmedEmail.substring(0, atIndex) : "";
-    // const newUser = {
-    //   email: trimmedEmail,
-    //   password: hashPassword,
-    //   username: userName,
-    // };
-    // const existingUser = await User.findOne({ email: trimmedEmail });
-    // if (existingUser) {
-    //   return res.status(409).json({
-    //     errCode: 1,
-    //     message: "Email already exists!",
-    //   });
-    // }
-    // const createdUser = await User.create(newUser);
-    // return res.status(201).json({
-    //   errCode: 0,
-    //   message: "Registration successful!",
-    //   data: createdUser,
-    // });
-    // } catch (error) {
-    //   return res.status(500).json({
-    //     errCode: 1,
-    //     message: "Internal server error!",
-    //     error: error.message,
-    //   });
-    // }
-  },
+  // getUserById: async (req, res) => {
+  //   try {
+  //     const user = await User.findById(req.params.id);
+  //     res.status(200).json(user);
+  //   } catch (err) {
+  //     res.status(500).json(err);
+  //   }
+  // },
+  // //REGISTER
+  // registerUser: async (req, res) => {
+  //   // try {
+  //   // const { email, password } = req?.body;
+  //   // const trimmedEmail = email?.trim();
+  //   // const trimmedPassword = password?.trim();
+  //   // const isEmailValid = trimmedEmail && regexEmail.test(trimmedEmail);
+  //   // const isPasswordValid =
+  //   //   trimmedPassword && trimmedPassword.match(regexPassword);
+  //   // if (!isEmailValid) {
+  //   //   return res.status(400).json({
+  //   //     errCode: 1,
+  //   //     message: "Invalid email format!",
+  //   //   });
+  //   // }
+  //   // if (!isPasswordValid) {
+  //   //   return res.status(400).json({
+  //   //     errCode: 1,
+  //   //     message: "Password must be at least 6 characters long.",
+  //   //   });
+  //   // }
+  //   // const salt = await bcrypt.genSaltSync(10);
+  //   // const hashPassword = await bcrypt.hashSync(trimmedPassword, salt);
+  //   // const atIndex = trimmedEmail.indexOf("@");
+  //   // const userName = atIndex !== -1 ? trimmedEmail.substring(0, atIndex) : "";
+  //   // const newUser = {
+  //   //   email: trimmedEmail,
+  //   //   password: hashPassword,
+  //   //   username: userName,
+  //   // };
+  //   // const existingUser = await User.findOne({ email: trimmedEmail });
+  //   // if (existingUser) {
+  //   //   return res.status(409).json({
+  //   //     errCode: 1,
+  //   //     message: "Email already exists!",
+  //   //   });
+  //   // }
+  //   // const createdUser = await User.create(newUser);
+  //   // return res.status(201).json({
+  //   //   errCode: 0,
+  //   //   message: "Registration successful!",
+  //   //   data: createdUser,
+  //   // });
+  //   // } catch (error) {
+  //   //   return res.status(500).json({
+  //   //     errCode: 1,
+  //   //     message: "Internal server error!",
+  //   //     error: error.message,
+  //   //   });
+  //   // }
+  // },
   // loginUser: async (req, res) => {
   //   try {
   //     const { email, password } = req.body;
